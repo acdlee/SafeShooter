84dba07742b7afb683e5aea1c2a6c994a7f0088a
Christopher Lee <christopherlee@wireless-10-104-179-111.umd.edu>
2019-09-20 09:03:20 -0400

    Refactoring after Player(Hero) code

--- SafeShooter.py
+++ SafeShooter.py
@@ -1,8 +1,8 @@
 import pygame
 import sys
 from gamesettings import GameSettings as settings
-from gamesettings import draw_stars
 from Player import Player
+from illustrate import draw_to_screen
 
 #initialize game engine
 pygame.init()
@@ -23,15 +23,8 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 
-	#Drawing the screen
-	screen.fill([90,14,118])
-	draw_stars(screen, game_settings.stars)
-	screen.blit(game_settings.bg.image, game_settings.bg.rect)
-	screen.blit(hero.image, hero.rect)
-
-	#Flip the display and tick the game clock
-	pygame.display.flip()
-	clock.tick(game_settings.clock_tick_rate)
+	#Once finished check for events, let's update the screen
+	draw_to_screen(screen, game_settings, hero, clock)
 
 
 #Quit after exting the main game loop
---
+++ illustrate.py
@@ -0,0 +1,13 @@
+import pygame
+from gamesettings import draw_stars
+
+def draw_to_screen(screen, game_settings, hero, clock):
+	#Drawing the screen
+	screen.fill([90,14,118])
+	draw_stars(screen, game_settings.stars)
+	screen.blit(game_settings.bg.image, game_settings.bg.rect)
+	screen.blit(hero.image, hero.rect)
+
+	#Flip the display and tick the game clock
+	pygame.display.flip()
+	clock.tick(game_settings.clock_tick_rate)
